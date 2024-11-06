f5a9a40c75df8ce29b693e5c44467df2f699932d
harismln22 <harismln205@gmail.com>
2024-11-06 21:36:45 +0700

    new files, crud books

--- application/controllers/Books.php
+++ application/controllers/Books.php
@@ -12,16 +12,88 @@ class Books extends CI_Controller
         $this->load->view('books/books', $data);
         $this->load->view('templates/footer');
     }
-
+    
     public function details($id)
     {
         $data['title_page'] = 'Detail Book';
         $data['book'] = $this->Book_model->getBookById($id);
-
-        // var_dump($data['book']);
-
+        $data['booklist'] = $this->Book_model->getBooks();
         $this->load->view('templates/header', $data);
         $this->load->view('books/detail', $data);
         $this->load->view('templates/footer');
     }
+
+    // Form Create
+    public function create()
+    {
+        $data['title_page'] = 'Add New Book';
+        $this->load->view('templates/header', $data);
+        $this->load->view('books/create', $data);
+        $this->load->view('templates/footer');
+    }
+
+    // Fungsi Simpan Data Baru
+    public function store()
+    {
+        $data = [
+            'title' => $this->input->post('title'),
+            'author' => $this->input->post('author'),
+            'description' => $this->input->post('description'),
+            'price' => (int)$this->input->post('price'),
+            'stock' => (int)$this->input->post('stock'),
+            'category' => $this->input->post('category'),
+            'publisher' => $this->input->post('publisher'),
+            'published_date' => date('Y-m-d'),
+            'ISBN' => $this->input->post('ISBN'),
+            'pages' => (int)$this->input->post('pages'),
+            'language' => $this->input->post('language'),
+            'cover_image' => $this->input->post('cover_image')
+        ];
+
+        $this->Book_model->createBook($data);
+        redirect('books');
+    }
+
+    // Fungsi Edit buku
+    public function edit($id)
+    {
+        $data['title_page'] = 'Edit Book';
+        $data['book'] = $this->Book_model->getBookById($id);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('books/edit', $data);
+        $this->load->view('templates/footer');
+    }
+
+    // Fungsi Update Buku
+    public function update($id)
+    {
+        $this->load->model('Book_model');
+        $data = [
+            'title' => $this->input->post('title'),
+            'author' => $this->input->post('author'),
+            'description' => $this->input->post('description'),
+            'price' => (float)$this->input->post('price'),
+            'stock' => (int)$this->input->post('stock'),
+            'category' => $this->input->post('category'),
+            'publisher' => $this->input->post('publisher'),
+            'published_date' => date('Y-m-d'),
+            'ISBN' => $this->input->post('ISBN'),
+            'pages' => (int)$this->input->post('pages'),
+            'language' => $this->input->post('language')
+        ];
+
+        // Update the book in the database
+        $this->Book_model->updateBook($id, $data);
+
+        redirect('books');
+    }
+
+    // Fungsi Hapus Buku
+    public function delete($id)
+    {
+        $this->Book_model->deleteBook($id);
+        redirect('books');
+    }
+
 }
--- application/models/Book_model.php
+++ application/models/Book_model.php
@@ -14,15 +14,32 @@ class Book_model extends CI_Model
     {
         $this->load->library('mongo_db');
         $data = $this->mongo_db->where(['_id' => new MongoDB\BSON\ObjectId($id)])->get('books');
-        return $data;
+        return $data[0];
 
     }
 
-    public function getBookByCartId($id)
+    // Fungsi Create Book
+    public function createBook($data)
     {
         $this->load->library('mongo_db');
-        $data = $this->mongo_db->where(['_id' => new MongoDB\BSON\ObjectId($id)])->get('books');
-        return $data[0];
+        return $this->mongo_db->insert('books', $data);
     }
 
+    // Fungsi Update Book
+    public function updateBook($id, $data)
+    {
+        $this->load->library('mongo_db');
+        return $this->mongo_db
+            ->where(['_id' => new MongoDB\BSON\ObjectId($id)])
+            ->set($data)
+            ->update('books');
+    }
+
+
+    // Fungsi Delete Book
+    public function deleteBook($id)
+    {
+        $this->load->library('mongo_db');
+        return $this->mongo_db->where(['_id' => new MongoDB\BSON\ObjectId($id)])->delete('books');
+    }
 }
